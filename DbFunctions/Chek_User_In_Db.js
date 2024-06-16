01bb5249abedf76d9ccbd642d1e3cb59da423d43
Vinit Pandit <vinitsharma2480@gmail.com>
2024-06-16 20:59:50 +0530

    food order logic , authentication middleware

--- DbFunctions/Chek_User_In_Db.js
+++ DbFunctions/Chek_User_In_Db.js
@@ -8,13 +8,27 @@ const path = require('path')
 //     Password: 'Password',
 //     Account_Type: 'Buyer'
 //   }
+
 async function Is_User_In_Db(data){
+    if (!data) {
+        return false
+    }
+    ['UserName' , 'Account_Type' , 'Password'].forEach((property)=>{
+        if (! property in data ){
+            return false;
+        }
+    })
+
+    
     const client = await getMongoClientInstance()
+    // console.log(data)
+    
     const collection = client.db('Pu_Mato').collection('User_Info')
-    const result = await collection.countDocuments({Account_Type : data.Account_Type , UserName : data.UserName , Password: data.Password})
+    const result = await collection.find({Account_Type : data.Account_Type , UserName : data.UserName , Password: data.Password}).toArray()
+    // const user = await result.toArray()
     console.log(result)
     await client.close()
-    return result == 1 
+    return result.length == 1 
 }
 
 module.exports = {
---
+++ utils/sendImage.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+async function sendImage() {
+    try {
+        // Read the image file synchronously
+        const image = fs.readFileSync(path.join(__dirname, './Img2.jpeg'));
+
+        // Convert the image data to base64
+        const imagedata = image.toString('base64');
+
+        // Prepare the payload with the base64 image data
+        const payload = {
+
+            userData: {
+                UserName: 'test',
+                Password: 'Password',
+                Account_Type: 'Buyer',
+            },
+            reqData : {
+                image: imagedata,
+
+            }
+
+        };
+
+        // Send the POST request with the image data
+        const result = await axios.post('http://192.168.1.17:8000/Set_Food_Items', payload);
+
+        // Log the response data
+        console.log(result.data);
+    } catch (err) {
+        // Log the error
+        console.error('Error sending image:', err.response ? err.response.data : err.message);
+    }
+}
+
+// Execute the function
+sendImage();
